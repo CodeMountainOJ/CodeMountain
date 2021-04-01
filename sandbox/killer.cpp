d0b224743306412d63e48b2bee54ecdf44877818
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-04-01 17:34:37 +0600

    Sandbox: Replace microseconds with seconds
    I'm silly AF. Microseconds is not milliseconds. And from now on, the
    killer thread will use seconds as the unit for measuring time

--- sandbox/killer.cpp
+++ sandbox/killer.cpp
@@ -8,7 +8,7 @@ killer::killer(config *sandbox_config, result *result_struct, pid_t victim)
     m_IsCleared = false;
     std::thread t([&] {
         if(m_IsCleared) return;
-        std::this_thread::sleep_for(std::chrono::microseconds(sandbox_config->time_limit * 1000));
+        std::this_thread::sleep_for(std::chrono::seconds(sandbox_config->time_limit));
         if(m_IsCleared) return;
         kill(victim, SIGKILL);
         result_struct->timeLimitExceeded = true;
