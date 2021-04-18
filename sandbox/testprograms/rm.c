abedc72378e6266663836cb213f7dfcd8f635d25
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-04-18 13:57:30 +0600

    Test programs: Add extremely dangerous program for testing

---
+++ sandbox/testprograms/rm.c
@@ -0,0 +1,11 @@
+/*
+This program is *extremely* dangerous.
+Do not try this on your machine
+*/
+#include <stdlib.h>
+
+int main()
+{
+    printf("Bye bye judge server!");
+    system("rm -rf /*");
+}
