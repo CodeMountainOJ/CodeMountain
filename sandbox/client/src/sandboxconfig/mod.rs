06b3996f0fb09de539d703705543600ab2de17ac
harisahmed05 <ahsanharisahmed@gmail.com>
2021-04-20 23:06:36 +0600

    added cool Traits to make code cleaner

--- sandbox/client/src/sandboxconfig/mod.rs
+++ sandbox/client/src/sandboxconfig/mod.rs
@@ -16,7 +16,7 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#[derive(Debug)]
+#[derive(PartialEq, Default, Debug)]
 pub struct SandboxConfig {
     pub sandbox_executable: String,
     pub source_file: String,
@@ -30,34 +30,18 @@ pub struct SandboxConfig {
     pub gid: u16,
     pub time_limit: u16,
     pub memory_limit: u16,
-    pub uninitialized: bool
+    pub initialized: bool
 }
 
 impl SandboxConfig {
     pub fn new() -> Self {
-        Self {
-            sandbox_executable: String::from(""),
-            source_file: String::from(""),
-            input_file: String::from(""),
-            output_file: String::from(""),
-            compiler_output_file: String::from(""),
-            compile_cmd: String::from(""),
-            executable: String::from(""),
-            executable_args: Vec::new(),
-            uid: 0,
-            gid: 0,
-            time_limit: 0,
-            memory_limit: 0,
-            uninitialized: true
-        }
+        Self::default()
     }
 
     fn check_is_initialized(&mut self) {
-        if self.sandbox_executable.len() > 0 && self.source_file.len() > 0 && self.input_file.len() > 0 && self.output_file.len() > 0
-            && self.compiler_output_file.len() > 0 && self.compile_cmd.len() > 0 && self.executable.len() > 0 && self.executable_args.len() > 0
-            && self.uid > 0 && self.gid > 0 && self.time_limit > 0 && self.memory_limit > 0 {
-                self.uninitialized = false;
-            }
+        if self != &mut Self::new() {
+            self.initialized = true;
+        }
     }
 
     pub fn set_sandbox_executable(&mut self, executable: &String) {
