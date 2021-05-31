488ad21642341b2bbe2b95598b1216ed3b2ddd34
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-05-31 10:43:03 +0600

    API Server: Add licenses :V

--- server/api/src/db/user/model.rs
+++ server/api/src/db/user/model.rs
@@ -1,3 +1,20 @@
+/* 
+ *  CodeMountain is a free and open source online judge open for everyone
+ *  Copyright (C) 2021 MD Gaziur Rahman Noor and contributors
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 use chrono::NaiveDateTime;
 use super::super::schema::users;
 
