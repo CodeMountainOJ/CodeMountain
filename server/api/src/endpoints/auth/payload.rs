d4c8c6e333dfe38d1f603b31260148d62567406d
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-06-02 15:39:12 +0600

    API Server: Fix compilation error
    Hint: It was a versioning issue

--- server/api/src/endpoints/auth/payload.rs
+++ server/api/src/endpoints/auth/payload.rs
@@ -15,7 +15,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-use actix_web_validator::Validate;
+use validator::Validate;
 use serde::{ Deserialize, Serialize };
 
 #[derive(Deserialize, Serialize, Validate)]
